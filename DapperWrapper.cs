5e5fd7ae8c48ae57133aa408ec859956ffcfd442
skip <sean@altsrc.net>
2016-06-28 10:50:19 -0700

    initial commit

---
+++ DapperWrapper.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Dapper;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	static class DapperWrapper
+	{
+		public static ICommand<T> Cmd<T>(string sql, object parameters) where T : class
+		{
+			return new DwCommand<T>(sql, parameters);
+		}
+	}
+
+	public interface ICommand<out T>
+		where T : class
+	{
+		T[] Execute(string connectionString);
+		ICommandMulti<Tnext> Cmd<Tnext>(string sql, object parameters) where Tnext : class;
+	}
+	internal interface ICommandAccessor<out T>
+		where T : class
+	{
+		T[] Execute(string connectionString);
+		ICommandMulti<Tnext> Cmd<Tnext>(string sql, object parameters) where Tnext : class;
+		T[] ExecuteConnection(SqlConnection openConnection);
+	}
+	internal class DwCommand<T> : ICommand<T>, ICommandAccessor<T>
+		where T : class
+	{
+		private readonly string Sql;
+		private readonly object Parameters;
+
+		internal DwCommand(string sql, object parameters)
+		{
+			this.Sql = sql;
+			this.Parameters = parameters;
+		}
+
+		public ICommandMulti<Tnext> Cmd<Tnext>(string sql, object parameters) where Tnext : class
+		{
+			return new DwCommandMulti<Tnext>(sql, parameters, this);
+		}
+
+		public T[] ExecuteConnection(SqlConnection openConnection)
+		{
+			return openConnection.Query<T>(Sql, Parameters).ToArray();
+		}
+
+		public T[] Execute(string connectionString)
+		{
+			using (var connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				return ExecuteConnection(connection);
+			}
+		}
+	}
+
+
+	public interface ICommandMulti<out T>
+		where T : class
+	{
+		object[][] Execute(string connectionString);
+		ICommandMulti<Tnext> Cmd<Tnext>(string sql, object parameters) where Tnext : class;
+	}
+	internal interface ICommandMultiAccessor
+	{
+		object[][] Execute(string connectionString);
+		object[] ExecuteLocal(SqlConnection openConnection);
+		ICommandAccessor<object> PreviousCommand { get; }
+		ICommandMultiAccessor PreviousMultiCommand { get; }
+	}
+	internal class DwCommandMulti<T> : ICommandMulti<T>, ICommandMultiAccessor
+		where T : class
+	{
+		private readonly string Sql;
+		private readonly object Parameters;
+		public ICommandAccessor<object> PreviousCommand { get; private set; }
+		public ICommandMultiAccessor PreviousMultiCommand { get; private set; }
+
+		internal DwCommandMulti(string sql, object parameters, ICommandAccessor<object> previousCommand)
+		{
+			this.Sql = sql;
+			this.Parameters = parameters;
+			this.PreviousCommand = previousCommand;
+		}
+
+		internal DwCommandMulti(string sql, object parameters, ICommandMultiAccessor previousCommand)
+		{
+			this.Sql = sql;
+			this.Parameters = parameters;
+			this.PreviousMultiCommand = previousCommand;
+		}
+
+		public ICommandMulti<Tnext> Cmd<Tnext>(string sql, object parameters) where Tnext : class
+		{
+			return new DwCommandMulti<Tnext>(sql, parameters, this);
+		}
+
+		public object[] ExecuteLocal(SqlConnection openConnection)
+		{
+			return openConnection.Query<T>(Sql, Parameters).ToArray();
+		}
+
+		public object[][] Execute(string connectionString)
+		{
+			var results = new List<object[]>();
+			var executionStack = new Stack<ICommandMultiAccessor>();
+
+			//unravel the stack of commands
+			ICommandMultiAccessor stackIndex = this;
+			//hack to execute that last single command....
+			ICommandAccessor<object> firstCommand = null;
+			while (stackIndex != null)
+			{
+				executionStack.Push(stackIndex);
+				if (stackIndex.PreviousMultiCommand == null)
+				{
+					firstCommand = stackIndex.PreviousCommand;
+				}
+				stackIndex = stackIndex.PreviousMultiCommand;
+			}
+
+			using (var connection = new SqlConnection(connectionString))
+			{
+				connection.Open();
+				results.Add(firstCommand.ExecuteConnection(connection));
+
+
+				while (executionStack.Count > 0)
+				{
+					results.Add(executionStack.Pop().ExecuteLocal(connection));
+				}
+			}
+
+			return results.ToArray();
+		}
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main()
+		{
+			var connectionString = @"";//removed for security purposes
+
+			var sql1 = @"SELECT 
+                            SurveyQuestionAnswerId AS Id
+                            ,SurveyQuestionId AS QuestionId
+                            ,AnswerText AS Text
+                            ,Sequence
+                            ,Score
+                            ,AllowTextComment AS AllowComment
+                            FROM SurveyQuestionAnswers
+                            WHERE SurveyQuestionAnswerId = @Id";
+			var parameters1 = new { id = 104 };
+
+			var sql2 = @"SELECT
+                            SurveyQuestionId AS Id
+                            ,Questiontext AS Text
+                            ,Sequence
+                            ,NumberAllowedResponses
+                            FROM SurveyQuestions
+                            WHERE SurveyId = @SurveyId";
+			var parameters2 = new { surveyId = 3 };
+
+
+			var results = DapperWrapper
+				.Cmd<AnswerModel>(sql1, parameters1)
+				.Cmd<QuestionModel>(sql2, parameters2)
+				.Cmd<QuestionModel>(sql2, parameters2)
+				.Execute(connectionString);
+
+			var results2 = DapperWrapper
+				.Cmd<AnswerModel>(sql1, parameters1)
+				.Execute(connectionString);
+
+			Console.ReadLine();
+
+		}
+
+		public class QuestionModel
+		{
+			public int? Id { get; set; }
+			public string Text { get; set; }
+			public uint NumberAllowedResponses { get; set; }
+
+			public int Sequence { get; set; }
+			public List<AnswerModel> Answers { get; set; }
+
+			public List<int> DependentAnswers { get; set; }
+
+			public QuestionModel()
+			{
+				this.Answers = new List<AnswerModel>();
+				this.DependentAnswers = new List<int>();
+			}
+		}
+
+		public class AnswerModel
+		{
+			public int? Id { get; set; }
+
+			public int? QuestionId { get; set; }
+
+			public int Sequence { get; set; }
+			public string Text { get; set; }
+			public decimal? Score { get; set; }
+			public bool AllowComment { get; set; }
+		}
+	}
+}
